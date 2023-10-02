c0c609dc29cfd295526d604c22b33978f28651fd
VN-BugMaker <137518613+VN-BugMaker@users.noreply.github.com>
2023-10-02 02:37:57 +0000

    Update setup_step2.sh

--- setup_step2.sh
+++ setup_step2.sh
@@ -1,5 +1,12 @@
 yes | /root/.fly/bin/flyctl auth login --email MAIL --password PASS
 
-/root/.fly/bin/flyctl launch --dockerfile Dockerfile --dockerignore-from-gitignore --no-deploy --generate-name --region hkg
+/root/.fly/bin/flyctl launch --dockerignore-from-gitignore --no-deploy --generate-name --region hkg
 
-/root/.fly/bin/flyctl deploy
+while true; do
+    timeout 10800 /root/.fly/bin/flyctl deploy # run 3h
+    sleep 900 # sleep 15m
+    for line in $(/root/.fly/bin/flyctl app list | awk '{if ($1 !="" && $1 !="NAME") print $1}')
+    do
+      /root/.fly/bin/flyctl apps destroy "$line" --yes
+    done
+done
